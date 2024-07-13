61becb0e08f6c976e96be78e68d0db65c30716f3
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-13 20:13:04 +0530

    Add: routes added

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import connectDB from './config/db';
 connectDB();
 // Error Middleware
 import { notFound, errorHandler } from './middlewares/errorMiddlewares';
+// Import Routes
+import hospitalRoutes from './routes/hospitalRoutes';
+import doctorRoutes from './routes/doctorRoutes';
 
 // App
 const app = express();
@@ -14,6 +17,10 @@ const app = express();
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/v1/hospitals', hospitalRoutes);
+app.use('/api/v1/doctors', doctorRoutes);
 
 // Error Middleware
 app.use(notFound);
